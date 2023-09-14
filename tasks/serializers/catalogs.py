a37a013c41d6fec4bd3b5364d92cb161951c45ee
RicardoWebProject <luisrijhn09@gmail.com>
2023-09-13 23:27:43 -0600

    update: actualizado serializer Status

--- tasks/serializers/catalogs.py
+++ tasks/serializers/catalogs.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import serializers
 #Modelos necesarios: (ordenados)
 ##Status, Servicios, Marca de Contador, Rutas, Aldeas/Pueblo, Poste eléctrico, Tipo de Identificación, Roles, Departamentos
@@ -15,7 +16,22 @@ from tasks.models.models import (Status,
 #TODO: Serializadores básicos para cada modelo catálogo, utilizando ModelSerializer
 
 class StatusSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Status
+        fields = ['id', 'status_name', 'insert_date', 'update_date']
+        read_only_fields = ['insert_date']
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['insert_date'] = instance.insert_date.strftime('%Y-%m-%d')
+        representation['update_date'] = instance.update_date.strftime('%Y-%m-%d') if instance.update_date is not None else instance.update_date
+        return representation
+    
+    def update(self, instance, validated_data):
+        instance.update_date = timezone.now()
+        instance.status_name = validated_data.get('status_name')
+        instance.save(update_fields=['status_name', 'update_date'])
+        return instance
 
 class ServiceSerializer(serializers.ModelSerializer):
     pass
